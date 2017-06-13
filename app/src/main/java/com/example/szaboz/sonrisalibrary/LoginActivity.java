f8211dedb8c014bfcf357290245d305b1179adf8
Zoltán János Szabó <szaboz@sonrisa.hu>
2017-06-13 16:24:18 +0200

    Load fragment dinamically

--- app/src/main/java/com/example/szaboz/sonrisalibrary/LoginActivity.java
+++ app/src/main/java/com/example/szaboz/sonrisalibrary/LoginActivity.java
@@ -1,14 +1,27 @@
 package com.example.szaboz.sonrisalibrary;
 
 import android.support.v4.app.FragmentActivity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class LoginActivity extends FragmentActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        if (findViewById(R.id.fragment_container) != null){
+            if (savedInstanceState != null){
+                return;
+            }
+            LoginFragment loginFragment = new LoginFragment();
+            loginFragment.setArguments(getIntent().getExtras());
+
+            getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, loginFragment).commit();
+        }
+
+
     }
+
+
+
 }
--- app/src/main/java/com/example/szaboz/sonrisalibrary/LoginFragment.java
+++ app/src/main/java/com/example/szaboz/sonrisalibrary/LoginFragment.java
@@ -1,12 +1,16 @@
 package com.example.szaboz.sonrisalibrary;
 
 import android.os.Bundle;
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class LoginFragment extends Fragment {
+    public static LoginFragment newInstance() {
+        return new LoginFragment();
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
