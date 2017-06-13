a6021784efb9176d5b4c648936604e798aed2f1a
Zoltán János Szabó <szaboz@sonrisa.hu>
2017-06-13 14:58:28 +0200

    Initial commit of project

---
+++ app/src/main/java/com/example/szaboz/sonrisalibrary/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.example.szaboz.sonrisalibrary;
+
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
