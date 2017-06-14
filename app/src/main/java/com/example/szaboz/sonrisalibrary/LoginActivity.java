3ed89b5072c17b5d3beb95d21dd5f087d64318dc
Zoltán János Szabó <szaboz@sonrisa.hu>
2017-06-14 16:43:13 +0200

    Add appbar

--- app/src/main/java/com/example/szaboz/sonrisalibrary/LoginActivity.java
+++ app/src/main/java/com/example/szaboz/sonrisalibrary/LoginActivity.java
@@ -1,10 +1,10 @@
 package com.example.szaboz.sonrisalibrary;
 
 import android.app.FragmentTransaction;
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class LoginActivity extends FragmentActivity implements LoginFragment.ClickedOnSignUpListener {
+public class LoginActivity extends AppCompatActivity implements LoginFragment.ClickedOnSignUpListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
